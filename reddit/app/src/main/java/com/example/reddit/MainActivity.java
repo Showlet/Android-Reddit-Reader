d3aabb3e235c1930bde627f7c9fe6d043d86b5bd
Vincent <vedard@gmail.com>
2016-01-29 12:57:51 -0500

    Add event on listview, add webview

--- reddit/app/src/main/java/com/example/reddit/MainActivity.java
+++ reddit/app/src/main/java/com/example/reddit/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.reddit;
 
+import android.content.Intent;
 import android.net.http.HttpResponseCache;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -32,6 +35,23 @@ public class MainActivity extends AppCompatActivity {
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         _recyclelst_post.setLayoutManager(llm);
 
+        _recyclelst_post.addOnItemTouchListener(new RecyclerEventListener(this, _recyclelst_post, new RecyclerEventListener.IEventListener() {
+            @Override
+            public void onClick(View view, int position) {
+                WebView webView = (WebView) findViewById(R.id.WebView);
+                String url = ((PostAdapter)_recyclelst_post.getAdapter()).getItem(position).url;
+                webView.loadUrl(url);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+            }
+
+            @Override
+            public void onDoubleTap(View view, int position) {
+            }
+        }));
+
         _swipe_layout.setColorSchemeResources(R.color.colorPrimary);
         _swipe_layout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener() {
@@ -48,9 +68,10 @@ public class MainActivity extends AppCompatActivity {
     {
         try {
             File httpCacheDir = new File(getApplicationContext().getCacheDir(), "http");
-            long httpCacheSize = 10 * 1024 * 1024;
+            long httpCacheSize = 100 * 1024 * 1024; // 100mb
             HttpResponseCache.install(httpCacheDir, httpCacheSize);
         } catch (IOException ex) {
+            Toast.makeText(this, "Unable to set Cache",Toast.LENGTH_LONG);
         }
     }
 
