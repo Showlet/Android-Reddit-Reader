7a886d2e04a5f5198b6f08e2e957b6ba20c6cda2
Vincent <vedard@gmail.com>
2016-02-01 09:27:23 -0500

    Add 2 missing file

---
+++ reddit/app/src/main/java/com/example/reddit/utilities/FetchCompleted.java
@@ -0,0 +1,14 @@
+package com.example.reddit.utilities;
+
+/**
+ * Created by vincent on 10/23/15.
+ */
+public abstract class FetchCompleted implements Runnable
+{
+    protected String m_result;
+
+    public void setResult(String result)
+    {
+        m_result = result;
+    }
+}
